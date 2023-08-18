271edeb04c9a24f1413ac6117607ea8f941f9444
Elizabeth <elizafoam@gmail.com>
2023-08-18 14:49:32 -0400

    create function for grid

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function createGrid(){
-  
-
+function upgradeGridLabel(){
+  gridLabel.textContent = 'Grid Size: ' + gridSize.value;
 }
+
+function upgradeGridSize(){
+  gridContainer.innerHTML = '';
+  for(let i = 0; i < gridSize.value; i++){
+    let gridCol = document.createElement('div');
+    gridCol.classList.add('col');
+    gridContainer.appendChild(gridCol);
+
+    for(let j = 0; j < gridSize.value; j++){
+      let gridSquare = document.createElement('div');
+      gridSquare.classList.add('row');
+      gridCol.appendChild(gridSquare);
+    }
+  }
+}
+
+const gridContainer = document.getElementById('grid');
+const gridSize = document.getElementById('grid-size');
+const gridLabel = document.getElementById('size-label');
+
+gridSize.addEventListener('mousemove', upgradeGridLabel);
+gridSize.addEventListener('change', upgradeGridSize);
