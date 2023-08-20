021ab36563bd83d4352f78c031f416688d97680f
Elizabeth <elizafoam@gmail.com>
2023-08-19 22:05:53 -0400

    fix color mode function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function upgradeGridLabel(){
+function setBoard(){
+  const gridSquares = document.querySelectorAll('.row');
+  gridSquares.forEach(gridSquare => gridSquare.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = penPicker.value;
+  }));
+}
+
+function setGridLabel(){
   gridLabel.textContent = 'Grid Size: ' + gridSize.value;
 }
 
-function upgradeGridSize(){
+function setGridSize(){
   gridContainer.innerHTML = '';
   for(let i = 0; i < gridSize.value; i++){
     let gridCol = document.createElement('div');
@@ -12,6 +19,9 @@ function upgradeGridSize(){
     for(let j = 0; j < gridSize.value; j++){
       let gridSquare = document.createElement('div');
       gridSquare.classList.add('row');
+      gridSquare.addEventListener('click', (e) => {
+        e.target.style.backgroundColor = penPicker.value;
+      });
       gridCol.appendChild(gridSquare);
     }
   }
@@ -25,10 +35,14 @@ const gridContainer = document.getElementById('grid');
 const gridSize = document.getElementById('grid-size');
 const gridLabel = document.getElementById('size-label');
 
-gridSize.addEventListener('mousemove', upgradeGridLabel);
-gridSize.addEventListener('change', upgradeGridSize);
+gridSize.addEventListener('mousemove', setGridLabel);
+gridSize.addEventListener('change', setGridSize);
 
 const penPicker = document.getElementById('color-picker');
 const backgroundPicker = document.getElementById('background-picker');
 
 backgroundPicker.addEventListener('change', setBackgroundColor);
+
+
+
+setBoard();
